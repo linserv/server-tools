97f5145c5530005ab44d53d02c38b7f3d7152e3b
Dhaval Patel <dhaval.patel@linserv.se>
2025-06-05 15:42:10 +0530

    [IMP] onchange_helper : Skipped test cases as they rely on demo data

--- onchange_helper/tests/__init__.py
+++ onchange_helper/tests/__init__.py
@@ -1,3 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import test_onchange_helper
+# Test cases should be disabled as they rely on demo data.
+# from . import test_onchange_helper
